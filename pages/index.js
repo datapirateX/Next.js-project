976427b418a4071df7e9826ff815823e6e9c1735
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-19 05:34:22 +0600

    Working with Links

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,9 @@ const DUMMY_MEETUPS = [
 ]
 
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+  return (
+      <MeetupList meetups={DUMMY_MEETUPS} />
+  );
 }
 
 export default HomePage;
