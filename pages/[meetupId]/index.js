d4a6c2025d1feda97b5cc9d2073abedbf3a6749e
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-21 02:48:54 +0600

    Update index.js

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
+
     return (
     <MeetupDetail image='https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png'
     title='First Meetup'
@@ -11,12 +12,18 @@ function MeetupDetails() {
     );
 }
 
-export async function getStaticProps() {
+export async function getStaticPaths() {
+    
+}
+
+export async function getStaticProps(context) {
+    
+    const meetupId = context.params.meetupId;
     return {
         Props: {
             meetupData: {
                 image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
-                id: 'm1',
+                id: meetupId,
                 title: 'First Meetup',
                 address: 'Some Street 5, Some city',
                 description: 'This is a first meetup',
