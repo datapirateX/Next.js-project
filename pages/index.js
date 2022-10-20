a6721e1ecc5787b75d6d50193bcbf0a2d64eb536
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-21 02:22:01 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,21 +27,23 @@ function HomePage(props) {
   );
 }
 
-export async function getServerSideProps(context) {
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
   return {
     props: {
       meetups: DUMMY_MEETUPS
     }
   }
+
 }
-
-// export async function getStaticProps() {
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS
-//     }
-//   }
-
-// }
 
 export default HomePage;
