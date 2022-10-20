db3e4c82aa1e2f5b59bf3f8198d3e2621aadb05f
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-21 02:28:28 +0600

    Update index.js

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,19 @@ function MeetupDetails() {
     );
 }
 
+export async function getStaticProps() {
+    return {
+        Props: {
+            meetupData: {
+                image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+                id: 'm1',
+                title: 'First Meetup',
+                address: 'Some Street 5, Some city',
+                description: 'This is a first meetup',
+
+            },
+        },
+    };
+}
+
 export default MeetupDetails;
