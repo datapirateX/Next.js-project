8ecee7b8b3b07a92be7af19898fef67c1506c931
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-22 01:57:58 +0600

    working with API

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,35 +2,48 @@ import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
-
-    return (
-    <MeetupDetail image='https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png'
-    title='First Meetup'
-    address='Some Street 5, Some City'
-    description='The meetup description'
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
     />
-    );
+  );
 }
 
 export async function getStaticPaths() {
-    
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
 }
 
 export async function getStaticProps(context) {
-    
-    const meetupId = context.params.meetupId;
-    return {
-        Props: {
-            meetupData: {
-                image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
-                id: meetupId,
-                title: 'First Meetup',
-                address: 'Some Street 5, Some city',
-                description: 'This is a first meetup',
-
-            },
-        },
-    };
+  const meetupId = context.params.meetupId;
+  return {
+    Props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some city",
+        description: "This is a first meetup",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
---
+++ pages/api/new-meetup.js
@@ -0,0 +1,9 @@
+function handler(req, res) {
+    if (req.method === 'POST') {
+        const data = req.body;
+
+        const { title, image, address, description } = data;
+    }
+}
+
+export default handler;
