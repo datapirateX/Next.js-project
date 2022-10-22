2530fc9aa9b4025ea5bd8091a3c72a847097b571
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-23 00:14:10 +0600

    working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,22 @@
-function handler(req, res) {
-    if (req.method === 'POST') {
-        const data = req.body;
+import { MongoClient } from "mongodb";
 
-        const { title, image, address, description } = data;
-    }
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://Mamun00339:vAWfei22V8JLeKpZ@cluster0.rins048.mongodb.net/meetups?retryWrites=true&w=majority");
+
+      const db = client.db();
+
+      const meetupsCollection = db.collection('meetups');
+      const result = await meetupsCollection.insertOne(data);
+
+      console.log(result);
+    
+  }
 }
 
 export default handler;
