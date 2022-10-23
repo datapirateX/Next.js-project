6b4f54e9958154f4a7440057fcc0913206c273e4
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-24 01:34:47 +0600

    Working with MongoDB

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,27 @@
-import { useEffect, useState } from "react";
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
   {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg',
-    address: 'Some address 5, 123334 Some City',
-    description: 'This is a first meetup!'
-
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e4/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg",
+    address: "Some address 5, 123334 Some City",
+    description: "This is a first meetup!",
   },
   {
-    id: 'm2',
-    title: 'A second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Melting_Toe_of_Athabasca_Glacier.jpg',
-    address: 'Some address 5, 123334 Some City',
-    description: 'This is a second meetup!'
-
+    id: "m2",
+    title: "A second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/cf/Melting_Toe_of_Athabasca_Glacier.jpg",
+    address: "Some address 5, 123334 Some City",
+    description: "This is a second meetup!",
   },
-
-]
+];
 
 function HomePage(props) {
-  return (
-      <MeetupList meetups={props.meetups} />
-  );
+  return <MeetupList meetups={props.meetups} />;
 }
 
 // export async function getServerSideProps(context) {
@@ -38,12 +35,28 @@ function HomePage(props) {
 // }
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://Mamun00339:vAWfei22V8JLeKpZ@cluster0.rins048.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
-    }
-  }
-
+      meetups: meetups.map(meetup => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+  };
 }
 
 export default HomePage;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,6 +1,8 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm"
+import { useRouter } from "next/router";
 
 function NewMeetupPage() {
+    const router = useRouter();
     async function addMeetupHandler(enteredMeetupData) {
         const response = await fetch('/api/new-meetup', {
             method: 'POST',
@@ -11,6 +13,8 @@ function NewMeetupPage() {
         });
 
         const data = await response.json();
+
+        router.push('/')
         
     }
     return <NewMeetupForm onAddMeetup={addMeetupHandler} />
