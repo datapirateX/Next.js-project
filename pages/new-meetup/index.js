b72a21e0e8af4a85dc59e4fa5232c577a3cdadcc
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-24 01:05:43 +0600

    Update index.js

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,17 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
-    function addMeetupHandler(enteredMeetupData) {
-        console.log(enteredMeetupData);
+    async function addMeetupHandler(enteredMeetupData) {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                'Content-Type': 'applicaiton/json'
+            }
+        });
+
+        const data = await response.json();
+        
     }
     return <NewMeetupForm onAddMeetup={addMeetupHandler} />
 }
